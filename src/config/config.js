b0d121e0db67c70f4c83f9450becc3a9873870ea
MichelHMachado <michelhmachado@gmail.com>
2024-11-07 12:13:54 -0300

    chore: sequelize configs

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: process.env.DB_DIALECT,
@@ -21,7 +21,7 @@ module.exports = {
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: process.env.DB_DIALECT,
