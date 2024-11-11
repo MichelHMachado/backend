3ea53ac3de7175cb462744bfa12e8bc8cf8d391b
MichelHMachado <michelhmachado@gmail.com>
2024-11-11 18:19:32 -0300

    FEAT: Enable HTTPS with self-signed certificates

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 declare const module: any;
 
 async function bootstrap() {
   const configService = new ConfigService();
   const PORT = parseInt(configService.get<string>('PORT'), 10);
-  const app = await NestFactory.create(AppModule);
+  const ORIGIN = configService.get<string>('CORS_ORIGIN');
+
+  const httpsOptions = {
+    key: fs.readFileSync('../frontend/certificates/localhost-key.pem'),
+    cert: fs.readFileSync('../frontend/certificates/localhost.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ORIGIN,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
